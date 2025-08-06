2cda82893b366b51ebf8943178408f7f00f54652
Nathan-Sasa <Jonathanbikuta05@gmail.com>
2025-08-06 23:10:51 +0100

    feat: section des articles recents mise en avant dans la section d'accueil

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit, HostListener} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { fadeIn, fadeInDelay, fadeInDelayXl, fadeInDelayXxl } from '../../animation/animation';
 import { ScrollDownVisibleDirective } from '../../shared/directives/scroll-down/scroll-down-visible.directive';
+
 
 
 @Component({
@@ -12,7 +13,13 @@ import { ScrollDownVisibleDirective } from '../../shared/directives/scroll-down/
   styleUrl: './home.component.css',
   animations: [fadeIn, fadeInDelay, fadeInDelayXl, fadeInDelayXxl]
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+
+    @ViewChild('textRef') textElement!: ElementRef;
+    selectArticle : any = null;
+    isExpanded = true;
+    showToggle = false;
+    isLargeScreen = false;
 
     mindcastDependacies = [
         {
@@ -24,9 +31,8 @@ export class HomeComponent {
     ]
 
 
-    // @ViewChild('commentaireBody') commentaireBody!: ElementRef;
+    // le declencheur de scroll down 
     @ViewChild('scrollDownVisible') scrollDownVisible!: ElementRef
-
     scrollDown() {
         if (this.scrollDownVisible) {
             this.scrollDownVisible.nativeElement.scrollIntoView({
@@ -36,21 +42,62 @@ export class HomeComponent {
         }
     }
 
+
+
+    // les articles à afficher en avant-premier
+
     articles = [
         {
-            image: 'assets/images/articles/article1.jpg',
-            title: 'Comment créer un site web avec Angular',
+            image: 'assets/images/articles/wordpress.jpg',
+            title: 'Comment créer un site web moderne et performant avec Angular',
             description: 'Découvrez comment créer un site web moderne et performant avec Angular, le framework JavaScript populaire.',
+            date: 'Publié le 7 mai 2025',
+            tag: 'tech',
+            like: '123',
+            comment: '45',
+            share: '67',
         },
         {
-            image: 'assets/images/articles/article1.jpg',
-            title: 'Comment créer un site web avec Angular',
-            description: 'Découvrez comment créer un site web moderne et performant avec Angular, le framework JavaScript populaire.',
+            image: 'assets/images/articles/instagram.jpg',
+            title: 'Les meilleures pratiques pour optimiser les performances d\'un site web',
+            description: 'Découvrez comment créer un site web moderne et performant avec Angular, le framework JavaScript populaire. Découvrez comment créer un site web moderne et performant avec Angular, le framework JavaScript populaire.',
+            date: 'Publié le 2 juin 2025',
+            tag: 'Reseaux',
+            like: '333',
+            comment: '245',
+            share: '51',
         },
         {
-            image: 'assets/images/articles/article1.jpg',
-            title: 'Comment créer un site web avec Angular',
-            description: 'Découvrez comment créer un site web moderne et performant avec Angular, le framework JavaScript populaire.',
+            image: 'assets/images/articles/student.jpg',
+            title: 'La gestion de la santé mentale avec Mindcast',
+            description: 'Est une application web de gestion de la santé mentale, qui permet aux utilisateurs de suivre leur humeur, de gérer leur stress et d\'améliorer leur bien être mental. L\'application propose des fonctionnalités telles que la journalisation de l\'humeur, des exercices de respiration, des conseils sur la santé mentale et des ressources pour trouver de l\'aide professionnelle. est une application web de gestion de la santé mentale, qui permet aux utilisateurs de suivre leur humeur, de gérer leur stress et d\'améliorer leur bien-être mental. L\'application propose des fonctionnalités telles que la journalisation de l\'humeur, des exercices de respiration, des conseils sur la santé mentale et des ressources pour trouver de l\'aide professionnelle. qui permet aux utilisateurs de suivre leur humeur, de gérer leur stress et d\'améliorer leur bien être mental. L\'application propose des fonctionnalités telles que la journalisation de l\'humeur, des exercices de respiration, des conseils sur la santé mentale et des ressources pour trouver de l\'aide professionnelle. est une application web de gestion de la santé mentale, qui permet aux utilisateurs de suivre leur humeur, de gérer leur stress et d\'améliorer leur bien-être mental. L\'application propose des fonctionnalités telles que la journalisation de l\'humeur, des exercices de respiration, des conseils sur la santé mentale et des ressources pour trouver de l\'aide professionnelle.',
+            date: 'Publié le 17 mai 2025',
+            tag: 'Education',
+            like: '223',
+            comment: '145',
+            share: '67',
         },
     ]
+
+    // la fonction pour afficher et masquer le text de la description dépassant line-clamp-5 l'article sélectionné
+    
+
+    // la fonction pour afficher le bouton voir plus ou masquer qui si le text dépasse 5 lignes
+    ngAfterViewInit(): void {
+        this.updateVisbility()
+    }
+    @HostListener('window:resize')
+    onResize() {
+        this.updateVisbility()
+    }
+    updateVisbility(){
+        const el = this.textElement?.nativeElement;
+        this.isLargeScreen = window.innerWidth >= 1024 //le breakpoint 
+        const isOverflowing = el.scrollHeight > el.clientHeight;
+        this.showToggle = !this.isLargeScreen && isOverflowing;
+    }
+
+    toggleText(){
+        this.isExpanded = !this.isExpanded;
+    }
 }
